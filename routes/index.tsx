8f2bbf03474604b1e559c40e2b18b4d88cd77775
Nicolas Sauvage <nicolas.sauvage@renaissance.com>
2023-03-07 21:20:40 +0100

    💄improve UI
    Full screen background!
    Total time: 2h

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,7 +4,7 @@ import getWeek from "https://deno.land/x/date_fns@v2.22.1/getWeek/index.ts";
 
 const getPairs = (team: string[], offset: number) => {
   const pairs: string[][] = [];
-  for (let i = 0; i < team.length; i += 1) {
+  for (let i = 0; i < team.length; i++) {
     const index2 = (team.length - i + offset) % team.length;
     if (pairs.every((p) => p[1] !== team[i]))
       pairs.push([team[i], team[Math.abs(index2)]]);
@@ -22,19 +22,19 @@ export default function Home(props: PageProps) {
   return (
     <>
       <Head>
-        <title>OoO Planner</title>
+        <title>Team 1:1</title>
       </Head>
-      <div class="max-w-screen max-h-full bg-yellow text-primary">
+      <div class="max-w-screen h-screen  bg-yellow text-primary">
         <div class="p-4 mx-auto max-w-screen-md bg-orange">
           <img src="/logo.png" class="pt-16" alt="logo" />
-          <p class="pt-2">Team members: {team.join(", ")}</p>
+          <p class="pt-8">Team members: {team.join(", ")}</p>
           <p class="pt-2">
-            The current week is {getWeek(new Date()) + manualOffset}
+            The current week number is {getWeek(new Date()) + manualOffset}
           </p>
           <div class="p-8 flex flex-col items-center">
             {getPairs(team, offset).map((pair) => {
               return (
-                <div class="p-1">
+                <div class="p-1 text-xl">
                   {pair[0]} - {pair[1]}
                 </div>
               );
